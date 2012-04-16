1d6eb01f2aa8027513ebc00bc8e20de0a46f7eab
Ivan Derevyanko <drussilla7@gmail.com>
2012-04-16 21:57:38 +0300

    ui updated

--- trunk/ui_transactiondialog.h
+++ trunk/ui_transactiondialog.h
@@ -1,8 +1,8 @@
 /********************************************************************************
 ** Form generated from reading UI file 'transactiondialog.ui'
 **
-** Created: Mon 16. Apr 16:56:19 2012
-**      by: Qt User Interface Compiler version 4.8.0
+** Created: Mon Apr 16 21:56:15 2012
+**      by: Qt User Interface Compiler version 4.7.4
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
